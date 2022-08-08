57efbb121b92ab81e986a74c8af96f936aed0667
TechnoMarc3 <77102150+TechnoMarc3@users.noreply.github.com>
2022-08-08 14:44:46 +0200

    Updated now playing command

--- src/main/Linux3000/commands/NowPlayingCommand.java
+++ src/main/Linux3000/commands/NowPlayingCommand.java
@@ -8,6 +8,7 @@ import main.Linux3000.audio.PlayerManager;
 import main.Linux3000.commands.types.ServerCommand;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class NowPlayingCommand implements ServerCommand{
@@ -20,6 +21,8 @@ public class NowPlayingCommand implements ServerCommand{
         final AudioPlayer audioPlayer = musicManager.audioPlayer;
         final AudioTrack track = audioPlayer.getPlayingTrack();
 
+
+
         if (track == null) {
             channel.sendMessage("Aktuell wird kein Titel abgespielt").queue();
             return;
@@ -28,6 +31,9 @@ public class NowPlayingCommand implements ServerCommand{
         final AudioTrackInfo info = track.getInfo();
 
         channel.sendMessageFormat("Aktuell spielt  `%s` von `%s` (Link: <%s>)", info.title, info.author, info.uri).queue();
+
+        channel.sendMessage(formatTime(track.getPosition()) + "  (** " + getLength(track) + " **)  " + formatTime(track.getDuration())).queue();
+
     }
 
     @Override
@@ -38,6 +44,42 @@ public class NowPlayingCommand implements ServerCommand{
     @Override
     public String name() {
         return "playing";
+    }
+
+    private String formatTime(long timeInMillis) {
+        long seconds = timeInMillis / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+
+        minutes %= 60;
+        seconds %=60;
+
+
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private String getLength(AudioTrack track) {
+        String s = "";
+        long duration = track.getDuration();
+        long currentPos = track.getPosition();
+        long seconds = duration / 1000;
+        long currentSeconds = currentPos / 1000;
+
+
+        for(int i = 0; i< seconds; i++) {
+            if(i % 10 == 0) {
+                if(currentSeconds <= i) {
+                    s += "-  ";
+                }else {
+                    s+= "# ";
+                }
+            }
+        }
+        return s;
+
+
+
     }
 }
 
